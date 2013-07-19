1a9b4f8833721a94eeaabf6ccb9b87150b690a0e
unemi <tatsuo.unemi@gmail.com>
2013-07-19 21:00:25 +0900

    Create pcolor.h

---
+++ pcolor.h
@@ -0,0 +1,95 @@
+// Definition of Color (RGB) for part names
+// Part#1 ... Soprano1 = red
+#define Part1R 0xdddd
+#define Part1G 0x0000
+#define Part1B 0x0000
+// Part#2 ... Soprano2
+#define Part2R 0xd7d7
+#define Part2G 0x2020
+#define Part2B 0x0000
+// Part#3 ... Soprano3
+#define Part3R 0xd1d1
+#define Part3G 0x5555
+#define Part3B 0x0000
+// Part#4 ... orange
+#define Part4R 0xcccc
+#define Part4G 0x7777
+#define Part4B 0x0000
+// Part#5
+#define Part5R 0xaaaa
+#define Part5G 0x7777
+#define Part5B 0x0000
+// Part#6
+#define Part6R 0x9999
+#define Part6G 0x7777
+#define Part6B 0x0000
+// Part#7 = dark yellow
+#define Part7R 0x7777
+#define Part7G 0x7777
+#define Part7B 0x0000
+// Part#8
+#define Part8R 0x5050
+#define Part8G 0x7777
+#define Part8B 0x0000
+// Part#9
+#define Part9R 0x2828
+#define Part9G 0x7777
+#define Part9B 0x0000
+// Part#10 = green
+#define PartAR 0x0000
+#define PartAG 0x7777
+#define PartAB 0x0000
+// Part#11
+#define PartBR 0x0000
+#define PartBG 0x7272
+#define PartBB 0x2222
+// Part#12
+#define PartCR 0x0000
+#define PartCG 0x6F6F
+#define PartCB 0x4444
+// Part#13 ... Bass = green blue
+#define PartDR 0x0000
+#define PartDG 0x6666
+#define PartDB 0x6666
+// Part#14
+#define  PartER 0x0000
+#define	PartEG 0x3333
+#define	PartEB 0x9999
+// Part#15 ... Piano = blue
+#define PartFR 0x0000
+#define PartFG 0x0000
+#define PartFB 0xcccc
+//
+// 16-20 Drums and Percussion
+// Part#P1
+#define PartP1R 0x3333
+#define PartP1G 0x0606
+#define PartP1B 0xaaaa
+// Part#P2
+#define PartP2R 0x4444
+#define PartP2G 0x0808
+#define PartP2B 0x9b9b
+// Part#P3
+#define PartP3R 0x5555
+#define PartP3G 0x0b0b
+#define PartP3B 0x8f8f
+// Part#P4
+#define PartP4R 0x6666
+#define PartP4G 0x0e0e
+#define PartP4B 0x8484
+// Part#P5 = violet
+#define PartP5R 0x7777
+#define PartP5G 0x1111
+#define PartP5B 0x7777
+// Part#P6
+#define PartP6R 0x6666
+#define PartP6G 0x1d1d
+#define PartP6B 0x6666
+// Part#P7
+#define PartP7R 0x5555
+#define PartP7G 0x2828
+#define PartP7B 0x5555
+// Part#P8 = gray
+#define PartP8R 0x4444
+#define PartP8G 0x4444
+#define PartP8B 0x4444
