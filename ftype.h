861f332eb7b407273e6dd2f4ac06fbdf4a54226c
unemi <tatsuo.unemi@gmail.com>
2013-07-19 20:59:51 +0900

    Create ftype.h

---
+++ ftype.h
@@ -0,0 +1,15 @@
+#ifdef  PLST
+#define	CreatorCode			"SBST"
+#define	FileTypeField		"sFl3"
+#define	FileTypeIntegrator	"sIt3"
+#define	FileTypeOptions		"sOp3"
+#define	CreatorCodeSMF		"TVOD"
+#define	FileTypeSMF			"Midi"
+#else
+#define	CreatorCode			'SBST'
+#define	FileTypeField		'sFl3'
+#define	FileTypeIntegrator	'sIt3'
+#define	FileTypeOptions		'sOp3'
+#define	CreatorCodeSMF		'TVOD'
+#define	FileTypeSMF			'Midi'
+#endif
