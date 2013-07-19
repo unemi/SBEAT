5370e72ad2d5395686e39c5744b12766c61dfe14
unemi <tatsuo.unemi@gmail.com>
2013-07-19 20:55:49 +0900

    Create timbre.h

---
+++ timbre.h
@@ -0,0 +1,8 @@
+enum {
+  diBassDrums, diHiHatPedal, diDsStick, diTomTom, diSnare, diHiHat,
+	diRideCymbal, diCrashCymbal, diConga, diBongo, diTimbale, diSurdo, diAgogo,
+	diWhistle, diGuillo, diWoodBlock, diCuica, diScratch, diTriangle,
+	diMetronome, diSticks, diHandClap, diCastanets, diTambourine,
+	diJingleBell, diBellTree, diCowbell, diVibraslap, diCabasa, diShaker,
+	diMaracas, diSlap, diClaves, diHighQ, diSqureClick
+};
