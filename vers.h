1575721b95ee93ce4347c948d1cde42452c79d39
unemi <tatsuo.unemi@gmail.com>
2013-07-19 21:02:05 +0900

    Create vers.h

---
+++ vers.h
@@ -0,0 +1,7 @@
+/* This "include file" is for the English version. */
+#define  MajorRevision	3
+#define	MinorRevision	2
+#define	ReleaseStage	beta
+#define	ReleaseNumber	1
+#define	ProductVersionShort	"3.2b1"
+#define	ProductVersionLong	"SBEAT 3.2b1 for MacOS Carbon"
